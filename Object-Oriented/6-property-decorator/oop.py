34863c87dd5b6bb18494e6578a4f0f4fdb3de074
Shittu Uthman <suthman20@gmail.com>
2021-07-29 10:57:51 -0700

    added an individual description with his age included

--- Object-Oriented/6-property-decorator/oop.py
+++ Object-Oriented/6-property-decorator/oop.py
@@ -1,10 +1,10 @@
 
 class Employee:
 
-    def __init__(self, first, last):
+    def __init__(self, first, last, age):
         self.first = first
         self.last = last
-
+        self.age = age
     @property
     def email(self):
         return '{}.{}@email.com'.format(self.first, self.last)
@@ -18,7 +18,10 @@ class Employee:
         first, last = name.split(' ')
         self.first = first
         self.last = last
-    
+
+    def individual_description(self):
+        return f"individual name is {self.first} {self.last}, and individual is {self.age} years old"
+
     @fullname.deleter
     def fullname(self):
         print('Delete Name!')
@@ -26,11 +29,13 @@ class Employee:
         self.last = None
 
 
-emp_1 = Employee('John', 'Smith')
+emp_1 = Employee('John', 'Smith', '19')
 emp_1.fullname = "Corey Schafer"
 
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
+print(emp_1.individual_description())
+
 
 del emp_1.fullname
